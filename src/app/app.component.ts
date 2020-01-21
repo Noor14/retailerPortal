2c56796310cd460025da43ef5b086126e3238e18
Noor Ali <noor.ali@haball.pk>
2020-01-21 16:17:37 +0500

    shared service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd , Event} from '@angular/router';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'retailer-portal';
+export class AppComponent implements OnInit {
+  constructor(private _router: Router, private _sharedService: SharedService) { }
+  ngOnInit() {
+    this._router.events.subscribe((event:Event) => {
+      if(event instanceof NavigationEnd && (event.url == '/registration' ||  event.url == '/login')){
+        this._sharedService.signUpBtnToggling.next(event.url);
+      }
+    });
+    
+  }
 }
