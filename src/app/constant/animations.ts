122ae1ba4bdb35f45f6004f0accf4bac13bc265d
Noor Ali <noor.ali@haball.pk>
2020-01-22 12:15:26 +0500

    animation implemented

---
+++ src/app/constant/animations.ts
@@ -0,0 +1,39 @@
+import { trigger, animate, transition, style, query, group, state } from '@angular/animations';
+
+export const routeAnimation =
+
+    trigger('fadeAnimation', [
+      transition('* => *', [
+        query(':enter', [
+                style({ opacity: 0 })
+            ], { optional: true }
+        ),
+        group([
+            query(':leave', [
+                    animate(300, style({ opacity: 0 }))
+                ],
+                { optional: true }
+            ),
+            query(':enter', [
+                    style({ opacity: 0 }),
+                    animate(300, style({ opacity: 1 }))
+                ],
+                { optional: true }
+            )
+        ])
+    ])]);
+
+// export const slideInOut =
+// trigger('slideInOut', [
+//   transition(':enter', [
+//     style({transform: 'translateX(100%)'}),
+//     animate('200ms ease-in', style({
+//       transform: 'translateX(0%)',
+//     }))
+//   ]),
+//   transition(':leave', [
+//     animate('200ms ease-in', style({
+//       transform: 'translateX(100%)',
+//     }))
+//   ])
+// ]);
