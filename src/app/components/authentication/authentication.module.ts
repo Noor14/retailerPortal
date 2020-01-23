91abfbc99568d39feb4aec830f08f76ba897e93e
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2020-01-23 13:19:49 +0500

    login module

--- src/app/components/authentication/authentication.module.ts
+++ src/app/components/authentication/authentication.module.ts
@@ -6,14 +6,17 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationComponent } from './authentication.component';
 import { HttpClientModule } from "@angular/common/http";
-
+import { FormsModule } from '@angular/forms';
+import { LoginService } from './login/login.service';
 
 @NgModule({
   declarations: [RegistrationComponent, LoginComponent, AuthenticationComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    AuthenticationRoutingModule
-  ]
+    AuthenticationRoutingModule,
+    FormsModule
+  ],
+  providers:[LoginService]
 })
 export class AuthenticationModule { }
