3906eb7893e455011981e879357dd1baf8388180
Noor Ali <noor.ali@haball.pk>
2020-01-23 12:53:46 +0500

    all authentication pages created

--- src/app/components/authentication/authentication.module.ts
+++ src/app/components/authentication/authentication.module.ts
@@ -6,10 +6,12 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationComponent } from './authentication.component';
 import { HttpClientModule } from "@angular/common/http";
+import { ForgetpassowrdComponent } from './forgetpassowrd/forgetpassowrd.component';
+import { UpdatepassowrdComponent } from './updatepassowrd/updatepassowrd.component';
 
 
 @NgModule({
-  declarations: [RegistrationComponent, LoginComponent, AuthenticationComponent],
+  declarations: [RegistrationComponent, LoginComponent, AuthenticationComponent, ForgetpassowrdComponent, UpdatepassowrdComponent],
   imports: [
     CommonModule,
     HttpClientModule,
---
+++ src/app/components/authentication/forgetpassowrd/forgetpassowrd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgetpassowrd',
+  templateUrl: './forgetpassowrd.component.html',
+  styleUrls: ['./forgetpassowrd.component.scss']
+})
+export class ForgetpassowrdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
