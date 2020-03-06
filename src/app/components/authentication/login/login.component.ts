91ced66efd6ada8f7e01fcd77284c7a1e2c79165
Noor Ali <noor.ali@haball.pk>
2020-03-06 16:49:31 +0500

    some changes

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -32,7 +32,9 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(){
-    this.loginFormSubscriber.unsubscribe();
+    if(this.loginFormSubscriber){
+      this.loginFormSubscriber.unsubscribe();
+    }
   }
 
   onChanges():void{
