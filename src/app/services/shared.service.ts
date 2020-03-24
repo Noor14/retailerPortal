deaddfe4409d819bb970ceac7e710eaa1f35f643
Noor Ali <noor.ali@haball.pk>
2020-03-24 19:24:00 +0500

    changes

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { baseApi } from '../constant/baseurl';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +11,8 @@ export class SharedService {
   private userInfo = new BehaviorSubject<any>(undefined);
   public getUserInfo = this.userInfo.asObservable();
 
-  constructor(private _http: HttpClient) { }
-  logoutUser() {
-    let promise = new Promise((resolve, reject) => {
-      const apiURL = `${baseApi}/api/users/logout`;
-      this._http.delete(apiURL)
-        .toPromise()
-        .then(res => {
-          resolve(res);
-        })
-        .catch(err => {
-          reject(err);
-        })
-    })
-    return promise;
-  }
+  constructor() { }
+
   setUser(data) {
     this.userInfo.next(data);
   }
