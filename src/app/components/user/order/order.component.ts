f1789b3f7dc25a01b9d39ac3f93352863cbdf8c2
Noor Ali <noor.ali@haball.pk>
2020-03-26 12:40:07 +0500

    changes

--- src/app/components/user/order/order.component.ts
+++ src/app/components/user/order/order.component.ts
@@ -21,293 +21,7 @@ export class OrderComponent implements OnInit, AfterViewInit {
   public companyDetailForm: FormGroup;
   public toggleCompanyProductList:boolean= false;
   public categoryList :TreeNode[]= [];
-  public files :TreeNode[]=	[
-    {
-      "data": {
-        "name": "Applications",
-        "size": "",
-        "type": ""
-      },
-      "children": [
-        {
-          "data": {
-            "name": "Angular",
-            "size": "25mb",
-            "type": "Folder"
-          }
-        },
-        {
-          "data": {
-            "name": "editor.app",
-            "size": "25mb",
-            "type": "Application"
-          }
-        },
-        {
-          "data": {
-            "name": "settings.app",
-            "size": "50mb",
-            "type": "Application"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Cloud",
-        "size": "20mb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "backup-1.zip",
-            "size": "10mb",
-            "type": "Zip"
-          }
-        },
-        {
-          "data": {
-            "name": "backup-2.zip",
-            "size": "10mb",
-            "type": "Zip"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Desktop",
-        "size": "150kb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "note-meeting.txt",
-            "size": "50kb",
-            "type": "Text"
-          }
-        },
-        {
-          "data": {
-            "name": "note-todo.txt",
-            "size": "100kb",
-            "type": "Text"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Documents",
-        "size": "75kb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "Work",
-            "size": "55kb",
-            "type": "Folder"
-          },
-          "children": [
-            {
-              "data": {
-                "name": "Expenses.doc",
-                "size": "30kb",
-                "type": "Document"
-              }
-            },
-            {
-              "data": {
-                "name": "Resume.doc",
-                "size": "25kb",
-                "type": "Resume"
-              }
-            }
-          ]
-        },
-        {
-          "data": {
-            "name": "Home",
-            "size": "20kb",
-            "type": "Folder"
-          },
-          "children": [
-            {
-              "data": {
-                "name": "Invoices",
-                "size": "20kb",
-                "type": "Text"
-              }
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Downloads",
-        "size": "25mb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "Spanish",
-            "size": "10mb",
-            "type": "Folder"
-          },
-          "children": [
-            {
-              "data": {
-                "name": "tutorial-a1.txt",
-                "size": "5mb",
-                "type": "Text"
-              }
-            },
-            {
-              "data": {
-                "name": "tutorial-a2.txt",
-                "size": "5mb",
-                "type": "Text"
-              }
-            }
-          ]
-        },
-        {
-          "data": {
-            "name": "Travel",
-            "size": "15mb",
-            "type": "Text"
-          },
-          "children": [
-            {
-              "data": {
-                "name": "Hotel.pdf",
-                "size": "10mb",
-                "type": "PDF"
-              }
-            },
-            {
-              "data": {
-                "name": "Flight.pdf",
-                "size": "5mb",
-                "type": "PDF"
-              }
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Main",
-        "size": "50mb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "bin",
-            "size": "50kb",
-            "type": "Link"
-          }
-        },
-        {
-          "data": {
-            "name": "etc",
-            "size": "100kb",
-            "type": "Link"
-          }
-        },
-        {
-          "data": {
-            "name": "var",
-            "size": "100kb",
-            "type": "Link"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Other",
-        "size": "5mb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "todo.txt",
-            "size": "3mb",
-            "type": "Text"
-          }
-        },
-        {
-          "data": {
-            "name": "logo.png",
-            "size": "2mb",
-            "type": "Picture"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Pictures",
-        "size": "150kb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "barcelona.jpg",
-            "size": "90kb",
-            "type": "Picture"
-          }
-        },
-        {
-          "data": {
-            "name": "primeng.png",
-            "size": "30kb",
-            "type": "Picture"
-          }
-        },
-        {
-          "data": {
-            "name": "prime.jpg",
-            "size": "30kb",
-            "type": "Picture"
-          }
-        }
-      ]
-    },
-    {
-      "data": {
-        "name": "Videos",
-        "size": "1500mb",
-        "type": "Folder"
-      },
-      "children": [
-        {
-          "data": {
-            "name": "primefaces.mkv",
-            "size": "1000mb",
-            "type": "Video"
-          }
-        },
-        {
-          "data": {
-            "name": "intro.avi",
-            "size": "500mb",
-            "type": "Video"
-          }
-        }
-      ]
-    }
-  ]
+ 
   constructor(private _orderDetailService : OrderDetailService) { }
 
   ngOnInit() {
@@ -326,18 +40,28 @@ export class OrderComponent implements OnInit, AfterViewInit {
 
   }
   ngAfterViewInit(){
-    let elemWrapper = document.getElementsByClassName('ui-treetable-wrapper')[0];
-    let elem = document.getElementsByClassName('ui-treetable-table')[0];
-    if(elemWrapper){
-      elemWrapper.classList.add('table-responsive');
-      elemWrapper.classList.add('gridArea');
-      elemWrapper.id="nav-scrollbar";
-    }
-    if(elem){
-      elem.classList.add('table')
+    this.setTableClassTree()
+  }
+  onTabChange(event){
+    if(event.activeId == "OrderSummary"){
+      this.setTableClassTree()
     }
   }
+  setTableClassTree(){
+      setTimeout(()=>{
+        let elemWrapper = document.getElementsByClassName('ui-treetable-wrapper')[0];
+        let elem = document.getElementsByClassName('ui-treetable-table')[0];
+        if(elemWrapper){
+          elemWrapper.classList.add('table-responsive');
+          elemWrapper.classList.add('gridArea');
+          elemWrapper.id="nav-scrollbar";
+        }
+        if(elem){
+          elem.classList.add('table')
+        }
+      },0)
 
+  }
   getKYCList(requestId){
     this.showSpinner=true;
     this._orderDetailService.getKYCListDetail('kyc/ConnectedKycList',requestId).then((data: any) => {
@@ -369,6 +93,7 @@ export class OrderComponent implements OnInit, AfterViewInit {
     })
   }
   generateProductCompany(data){
+    this.setTableClassTree();
     let list = [];
     let dataList = [];
     for (let index = 0; index < data.SubCategory.length; index++) {
