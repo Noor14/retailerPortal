b9bd5924eb78a265b3e39e01b9132833b989dc1b
Noor Ali <noor.ali@haball.pk>
2020-04-03 18:10:26 +0500

    view receipt done

--- src/app/components/user/payment-details/payment-detail.service.ts
+++ src/app/components/user/payment-details/payment-detail.service.ts
@@ -23,4 +23,5 @@ export class PaymentDetailService {
     })
     return promise;
   }
+
 }
--- src/app/components/user/payment-details/payment-details.component.ts
+++ src/app/components/user/payment-details/payment-details.component.ts
@@ -1,9 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { loadingConfig } from './../../../constant/globalfunction';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PaymentDetailService } from './payment-detail.service';
 import * as moment from 'moment';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-details',
@@ -20,7 +23,9 @@ export class PaymentDetailsComponent implements OnInit {
 
  constructor(
    private activatedRoute: ActivatedRoute,
-   private _paymentDetailService: PaymentDetailService
+   private _paymentDetailService: PaymentDetailService,
+   private _toast: ToastrService,
+   private _domSanitizer: DomSanitizer
    ) {
       this.requestId = this.activatedRoute.snapshot.url[1] && Number(this.activatedRoute.snapshot.url[1].path)
       this.requestType = this.activatedRoute.snapshot.url[2] && Number(this.activatedRoute.snapshot.url[2].path);
@@ -35,7 +40,6 @@ export class PaymentDetailsComponent implements OnInit {
         this.getPaymentDetails('invoices', this.requestId);
       }
     this.paymentDetailForm = new FormGroup({
-      ID: new FormControl(0),
       PrePaidNumber:  new FormControl({value:null, disabled:true}, [Validators.required]),
       PaidAmount: new FormControl({value:null, disabled:true}, [Validators.required]),
       CompanyName: new FormControl({value:null, disabled:true}, [Validators.required]),
@@ -62,9 +66,40 @@ export class PaymentDetailsComponent implements OnInit {
       this.paymentDetailForm.patchValue(data);
       this.showSpinner=false;
     })
-    .catch(err => {
+    .catch((err:HttpErrorResponse) => {
       this.showSpinner=false;
+      if(err.error){
+        this._toast.error(err.error.message, "Error")
+      }
     })
+  }
+  viewReceipt(){
+      this.showSpinner=true;
+      this._paymentDetailService.getDetail('prepaidrequests/paymentreceipt', this.requestId)
+      .then((res:any)=>{
+        this.showSpinner=false;
+        if(res.data && res.data.length){
+          let typedArray = new Uint8Array(res.data);
+          const stringChar = typedArray.reduce((data, byte)=> {
+            return data + String.fromCharCode(byte);
+            }, '')
+          let base64String = btoa(stringChar);
+          let doc = this._domSanitizer.bypassSecurityTrustUrl(`data:application/octet-stream;base64, ${base64String}`) as string;
+          doc = this._domSanitizer.sanitize(SecurityContext.URL, doc) ;
+          const downloadLink = document.createElement("a");
+          const fileName = "receipt.pdf";
+          downloadLink.href = doc;
+          downloadLink.download = fileName;
+          downloadLink.click();
+        }
+  
+      })
+      .catch((err:HttpErrorResponse)=>{
+        this.showSpinner=false;
+          if(err.error){
+            this._toast.error(err.error.message, "Error")
+          }
+        })
   }
 
 }
