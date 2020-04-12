fb5d6e96158ce762daff5b21585d69e240ca20d9
Noor Ali <noor.ali@haball.pk>
2020-04-12 20:57:33 +0500

    changes animation insert

--- src/app/constant/animations.ts
+++ src/app/constant/animations.ts
@@ -1,26 +1,12 @@
 import { trigger, animate, transition, style, query, group, state } from '@angular/animations';
 
 export const fadeAnimation =
-    trigger('fadeAnimation', [
-      transition('* => *', [
-        query(':enter', [
-                style({ opacity: 0 })
-            ], { optional: true }
-        ),
-        group([
-            query(':leave', [
-                    animate(20, style({ opacity: 0 }))
-                ],
-                { optional: true }
-            ),
-            query(':enter', [
-                    style({ opacity: 0 }),
-                    animate(300, style({ opacity: 1 }))
-                ],
-                { optional: true }
-            )
-        ])
-    ])]);
+trigger('fadeAnimation', [ 
+  transition('void => *', [
+    style({ opacity: 0 }), 
+    animate(200, style({opacity: 1}))
+  ]) 
+])
 
 export const slideInOut =
 trigger('slideInOut', [
