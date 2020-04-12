cb47f41067e1814cb45a62c09360a0416b3accf6
Noor Ali <noor.ali@haball.pk>
2020-04-12 06:39:56 +0500

    internet Connectivity check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd , Event} from '@angular/router';
 import { SharedService } from './services/shared.service';
+import { ConnectionService } from 'ng-connection-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,22 @@ import { SharedService } from './services/shared.service';
 export class AppComponent implements OnInit {
   constructor(
     private _router: Router,
-    private _sharedService :SharedService) { }
+    private _sharedService :SharedService,
+    private _connectionService: ConnectionService,
+    private _toast: ToastrService
+   ) { }
   ngOnInit() {
     this._router.events.subscribe((event:Event) => {
       if(event instanceof NavigationEnd){
         this._sharedService.btnToggling.next(event.url);
       }
     });
-    
+    this._connectionService.monitor().subscribe(isConnected => {
+      if (!isConnected) {
+        this._toast.error("Internet connection lost")
+      }
+
+    })
   }
 
 }
