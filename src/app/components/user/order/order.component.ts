e6fe142bbafca19f0923ce24eb51167073ba9a75
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2020-05-15 23:25:59 +0500

    packsize calculation removval

--- src/app/components/user/order/order.component.ts
+++ src/app/components/user/order/order.component.ts
@@ -496,9 +496,9 @@ export class OrderComponent implements OnInit, AfterViewInit, OnDestroy, CanComp
     this.grossAmount = 0;
     this.totalDiscount = 0;
     this.orderSummary.filter(obj => obj.OrderQty).forEach(obj => {
-      this.netAmount += obj.OrderQty * (obj.ProductUnitPrice - obj.DiscountAmount) * obj.PackSize;
-      this.grossAmount += obj.ProductUnitPrice * obj.OrderQty * obj.PackSize;
-      this.totalDiscount += obj.DiscountAmount  * obj.OrderQty * obj.PackSize;
+      this.netAmount += obj.OrderQty * (obj.ProductUnitPrice - obj.DiscountAmount) ;
+      this.grossAmount += obj.ProductUnitPrice * obj.OrderQty  ;
+      this.totalDiscount += obj.DiscountAmount  * obj.OrderQty  ;
     });
   }
   deleteSummaryRow(index){
