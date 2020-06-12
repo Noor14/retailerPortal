c81a452c3ebab51673f51ca965a0495d837f0dab
Noor Ali <noor.ali@haball.pk>
2020-06-12 22:09:55 +0500

    changes

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -125,7 +125,7 @@ export class ProfileComponent implements OnInit, OnDestroy, CanComponentDeactiva
         this.onChangesAtnewPassChange();
         this.onChangesAtnewConfirmChange();
       }
-  
+      this.showSpinner = false;
     })
     .catch(err=>{
       this.showSpinner = false;
