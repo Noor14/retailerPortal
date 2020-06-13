f3f72140f0e33fbd2977686487ae77bfaec585b0
Noor Ali <noor.ali@haball.pk>
2020-06-13 14:45:15 +0500

    changes

--- src/app/services/role-authorization.service.ts
+++ src/app/services/role-authorization.service.ts
@@ -9,17 +9,16 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 export class RoleAuthorizationService {
   
   private userRoles: any;
-  constructor(private _sharedService : SharedService) {
-    this.userRoles = this._sharedService.getUserRole();
-    if(!this.userRoles){
-      let info = localStorage.getItem('userIdentity');
-      if(info){
-       this.userRoles = JSON.parse(info).UserAccount.UserRights;
-      }
-    }
-   }
+  constructor(private _sharedService : SharedService) {}
     
     isAuthorized() {
+      this.userRoles = this._sharedService.getUserRole();
+      if(!this.userRoles){
+        let info = localStorage.getItem('userIdentity');
+        if(info){
+         this.userRoles = JSON.parse(info).UserAccount.UserRights;
+        }
+      }
         return this.userRoles && this.userRoles.length && !!this.userRoles;
     }
 
