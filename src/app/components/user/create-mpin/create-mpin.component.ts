1dfa1e02a2985e472982e28a119d30d84cf640d5
maaz-haball <maaz.khan@haball.pk>
2020-07-08 17:21:29 +0500

    go back button

--- src/app/components/user/create-mpin/create-mpin.component.ts
+++ src/app/components/user/create-mpin/create-mpin.component.ts
@@ -1,3 +1,4 @@
+import { SharedService } from './../../../services/shared.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateMPINComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _sharedService: SharedService) { }
 
   ngOnInit() {
+  }
+  gotoBack(){
+    this._sharedService.setRenderComponent('linkedAccounts');
   }
 
 }
