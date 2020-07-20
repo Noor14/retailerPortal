adfab4116bd78bb13fff5cf1b532d0cd8e32add2
Noor Ali <noor.ali@haball.pk>
2020-07-21 04:41:22 +0500

    changes

--- src/app/components/user/accounts/pay-account/pay-account.component.ts
+++ src/app/components/user/accounts/pay-account/pay-account.component.ts
@@ -1,3 +1,5 @@
+import { loadingConfig } from './../../../../constant/globalfunction';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pay-account.component.scss']
 })
 export class PayAccountComponent implements OnInit {
+  public showSpinner: boolean;
+  public spinnerConfig: any;
+  public payAccountForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.spinnerConfig = loadingConfig;
+    this.payAccountForm = new FormGroup({
+      PrePaidNumber:  new FormControl({value:null, disabled:true}, [Validators.required]),
+      PaidAmount: new FormControl({value:null, disabled:true}, [Validators.required]),
+      CompanyName: new FormControl({value:null, disabled:true}, [Validators.required]),
+      CreatedDate: new FormControl({value:null, disabled:true}, [Validators.required]),
+      AuthID: new FormControl({value:null, disabled:true}, [Validators.required]),
+      BankName: new FormControl({value:null, disabled:true}, [Validators.required]),
+      TransactionDate: new FormControl({value:null, disabled:true}, [Validators.required]),
+      TransactionCharges: new FormControl({value:null, disabled:true}, [Validators.required]),
+      TotalAmount: new FormControl({value:null, disabled:true},[Validators.required])
+    });
+  }
+  pay(){
+    console.log("pay")
   }
 
 }
